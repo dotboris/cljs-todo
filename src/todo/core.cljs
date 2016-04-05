9a20aa36dee8a1a1f79f939cfc4a4aca05adf6b7
Boris Bera <bera.boris@gmail.com>
2016-04-05 18:58:23 -0400

    cleaned up whitespace in core file

--- src/todo/core.cljs
+++ src/todo/core.cljs
@@ -3,9 +3,7 @@
 (enable-console-print!)
 
 ;; define your app data so that it doesn't get over-written on reload
-
 (defonce app-state (atom {:text "Hello world!"}))
-
 
 (defn on-js-reload [])
   ;; optionally touch your app-state to force rerendering depending on
