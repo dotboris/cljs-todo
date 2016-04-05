ebf970d50b2ec08a46259dcea51bb3aad7a2ce4e
Boris Bera <bera.boris@gmail.com>
2016-04-05 18:56:20 -0400

    inital commit

---
+++ project.clj
@@ -0,0 +1,51 @@
+(defproject todo "0.1.0-SNAPSHOT"
+  :description (str "The ever loved TODO app. "
+                    "This is my first play around with clojure. "
+                    "It probably sucks!")
+  :url "https://github.com/dotboris/cljs-todo"
+  :license {:name "MIT"}
+
+  :min-lein-version "2.6.1"
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [org.clojure/core.async "0.2.374"
+                  :exclusions [org.clojure/tools.reader]]]
+
+  :plugins [[lein-figwheel "0.5.2"]
+            [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
+
+  :source-paths ["src"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
+
+                ;; If no code is to be run, set :figwheel true for continued automagical reloading
+                :figwheel {:on-jsload "todo.core/on-js-reload"}
+
+                :compiler {:main todo.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/todo.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :source-map-timestamp true}}
+               ;; This next build is an compressed minified build for
+               ;; production. You can build this with:
+               ;; lein cljsbuild once min
+               {:id "min"
+                :source-paths ["src"]
+                :compiler {:output-to "resources/public/js/compiled/todo.js"
+                           :main todo.core
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+
+  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
+             ;; :server-port 3449 ;; default
+             ;; :server-ip "127.0.0.1"
+
+             :css-dirs ["resources/public/css"] ;; watch and update CSS
+
+             ;; Start an nREPL server into the running figwheel process
+             :nrepl-port 7888})
