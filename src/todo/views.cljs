a827b05ee7d76048245370c59c531ecfe6cb70ed
Boris Bera <bera.boris@gmail.com>
2016-04-15 19:28:57 -0400

    Added a counter for done tasks
    It's kinda stupid since I can't remove it. Who finishes the thing they
    start anyways?

--- src/todo/views.cljs
+++ src/todo/views.cljs
@@ -13,8 +13,17 @@
     (dom/ul nil
       (om/build-all todo-item (:items list)))))
 
+(defn counter [list owner]
+  (om/component
+    (dom/p nil
+      "You have "
+      (dom/span #js {:className "count"}
+        (count (filter (comp not s/done?) (:items list))))
+      " things left to do. Get on with it!")))
+
 (defn todo-app [list owner]
   (om/component
     (dom/div #js {:className "container"}
       (dom/h1 nil "Todo")
+      (om/build counter list)
       (om/build todo-list list))))
